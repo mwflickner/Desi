c18ce774257823db4c0afa604031733105aafc5d
Matthew <mwflickner@gmail.com>
2015-05-25 15:45:40 -0700

    intial commit

---
+++ Desi/DesiGroup.swift
@@ -0,0 +1,16 @@
+//
+//  DesiGroup.swift
+//  Desi
+//
+//  Created by Matthew Flickner on 5/24/15.
+//  Copyright (c) 2015 Sweg. All rights reserved.
+//
+
+import UIKit
+
+class DesiGroup: NSObject {
+   var groupId = "1"
+   var groupName = "Fort Knox"
+   var users = []
+   var theDesi = "your mom"
+}
---
+++ Desi/DesiUser.swift
@@ -0,0 +1,17 @@
+//
+//  DesiUser.swift
+//  Desi
+//
+//  Created by Matthew Flickner on 5/24/15.
+//  Copyright (c) 2015 Sweg. All rights reserved.
+//
+
+import UIKit
+
+class DesiUser: NSObject {
+    var userName = "DJFlicky"
+    var password = "sweg"
+    var firstName = "Matthew"
+    var lastName = "Flickner"
+    var groupsIn = []
+}
