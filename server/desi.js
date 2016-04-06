4283d7f15f196c0893cdbf4a4ce039b5dfc1e2ab
Matthew <mwflickner@gmail.com>
2016-04-05 17:51:15 -0700

    https and http working

--- server/desi.js
+++ server/desi.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var fs = require('fs');
 var dotenv = require('dotenv').config();
+var http = require('http');
+var https = require('https');
 var ParseServer = require('parse-server').ParseServer;
+
+var desiKey = fs.readFileSync('server.key');
+var desiCert = fs.readFileSync('server.crt');
+var HTTP_PORT = process.env.HTTP_PORT;
+var HTTPS_PORT = process.env.HTTPS_PORT;
+
+var options = {
+  key: desiKey,
+  cert: desiCert
+};
+
 var app = express();
 
 var api = new ParseServer({
@@ -17,9 +30,21 @@ var api = new ParseServer({
   serverURL: process.env.API_URL // Don't forget to change to https if needed
 });
 
+// routes
+app.get('/hey', function(req, res) {
+    res.send('HEY!');
+});
+
 // Serve the Parse API on the /parse URL prefix
 app.use('/parse', api);
 
-app.listen(1337, function() {
-  console.log('Desi Server running on port 1337.');
+http.createServer(app).listen(HTTP_PORT, function(){
+  console.log("Desi Server running HTTP");
 });
+
+https.createServer(options, app).listen(HTTPS_PORT, function(){
+  console.log('Desi Server running HTTPS');
+});
+
+
+
